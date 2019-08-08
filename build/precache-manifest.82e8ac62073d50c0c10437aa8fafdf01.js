b63396186c6ba9edfb874abb211b80516bf495b0
zx <acczhuang@163.com>
2019-08-08 15:32:14 +0800

    bable

---
+++ build/precache-manifest.82e8ac62073d50c0c10437aa8fafdf01.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3d1d19072270cdcf74486fec4eb06c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "656f1ad6501efb50d99c",
+    "url": "./static/css/2.dae7720b.chunk.css"
+  },
+  {
+    "revision": "faa7212c8f459b47345e",
+    "url": "./static/css/main.35a2ef52.chunk.css"
+  },
+  {
+    "revision": "656f1ad6501efb50d99c",
+    "url": "./static/js/2.21dce181.chunk.js"
+  },
+  {
+    "revision": "faa7212c8f459b47345e",
+    "url": "./static/js/main.472ff5c8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "99666534c53a316e668da839c5702aad",
+    "url": "./static/media/baby-trolley.99666534.png"
+  },
+  {
+    "revision": "1fa76ce513c116de03fbe2a4b0fb6559",
+    "url": "./static/media/beach-parasol-water-.1fa76ce5.png"
+  },
+  {
+    "revision": "214fda87a445551bcb25e1dfeaf4df25",
+    "url": "./static/media/dating-rose.214fda87.png"
+  },
+  {
+    "revision": "38c555f56e242abf4964909a25593d7f",
+    "url": "./static/media/design-tool-pen-station.38c555f5.png"
+  },
+  {
+    "revision": "7832056b35f2d45e58f1a548add6f08e",
+    "url": "./static/media/fenxiang.7832056b.png"
+  },
+  {
+    "revision": "53b8d3fdac0aa527486921805b44eeaf",
+    "url": "./static/media/graph-stats-circle.53b8d3fd.png"
+  },
+  {
+    "revision": "ba30b6276c47bf758a3c495cb2275ff3",
+    "url": "./static/media/remen.ba30b627.png"
+  },
+  {
+    "revision": "0d2a183adcf8d06c6a6a9d9f5d3593c0",
+    "url": "./static/media/shoucang.0d2a183a.png"
+  },
+  {
+    "revision": "3a9c00066d0768ff42573568d4616df5",
+    "url": "./static/media/sousuo.3a9c0006.png"
+  },
+  {
+    "revision": "68da2d4747aa6375ce25b945b2ae9045",
+    "url": "./static/media/yuyin.68da2d47.png"
+  }
+]);
